30c2c66baeb43505fa1f38c7a07b5aefe8e055df
Rithvik09 <34558093+Rithvik09@users.noreply.github.com>
2021-04-02 22:14:58 -0400

    Update Event.java

--- Temple_Reminder1/src/Event.java
+++ Temple_Reminder1/src/Event.java
@@ -4,23 +4,9 @@ public class Event {
 	private static String Name;
 	private static String eventType;
 	private static Date date;
-	private static Date Edate;
-	private static Date Fdate;
-	static int cy = 1;
-	static int cm = 1;
-	public Event(String n, String e, Date d, Date ed) {
+	public Event(String n, Date d) {
 		Name = n;
-		eventType = e;
 		date = d;
-		Edate = ed;
-	}
-	
-	public Event(String n, String e, Date d, Date ed, Date fd) {
-		Name = n;
-		eventType = e;
-		date = d;
-		Edate = ed;
-		Fdate = fd;
 	}
 
 	public static void setName(String n) {
@@ -30,14 +16,6 @@ public class Event {
 	public static String getName() {
 		return Name;
 	}
-	
-	public static void setEventType(String e) {
-		Event.eventType = e;
-	}
-	
-	public static String getEventType() {
-		return eventType;
-	}
 
 	public static void setDate(Date d) {
 		Event.date = d;
@@ -45,39 +23,5 @@ public class Event {
 	
 	public static Date getDate() {
 		return date;
-	}
-	
-	public static void setEdate(Date ed) {
-		Event.Edate = ed;
-	}
-	
-	public static Date getEdate() {
-		return Edate;
-	}
-
-	public static void setFdate(Date fd) {
-		int d = Edate.getDate();
-		d = d - 10;
-		int y = date.getYear();
-		int m = Edate.getMonth() - 1;
-		if(Name.equals("Shreemandam")) {
-			m = m + cm;
-		}else {
-			y = y + cy;
-		}
-		fd = new Date(y, m, d);
-		Fdate = fd;
-	}
-	
-	public static Date getFdate() {
-		return Fdate;
-	}
-	
-	public static void setCY(int cy) {
-		cy++;
-	}
-	
-	public static void setCM(int cm) {
-		cm++;
 	}
 }
